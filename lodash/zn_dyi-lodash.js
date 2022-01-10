00174cd8fd503296ed1371971e5f7b2a0bf178b7
jcy <1294164535@qq.com>
2022-01-10 16:24:20 +0800

    chunk

--- lodash/zn_dyi-lodash.js
+++ lodash/zn_dyi-lodash.js
@@ -1,6 +1,14 @@
 var zn_dyi = {
   chunk: function (array, size) {
-
+    var len = array.length
+    var res = []
+    for (var i = 0; i < array.length; i++) {
+      var temp = []
+      for (var j = i; j < size; j++) {
+        temp.push(array[j])
+      }
+      res.push(temp)
+    }
+    return res
   }
-
 }
