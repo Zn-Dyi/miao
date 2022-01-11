fc374767985c39cab96fae4f224f0afceb520daf
jcy <1294164535@qq.com>
2022-01-11 08:18:35 +0800

    aaaa

--- lodash/zn_dyi-lodash.js
+++ lodash/zn_dyi-lodash.js
@@ -26,6 +26,17 @@ var zn_dyi = {
   },
 
 
+  difference: function (array, value) {
+    var res = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] in value) {
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+
   drop: function (array, n) {
     var res = []
     var ary = array
@@ -39,5 +50,111 @@ var zn_dyi = {
       res.push(ary[i])
     }
     return res
-  }
+  },
+
+
+  dropRight: function (array, n) {
+    var res = []
+    var ary = array
+    var len = ary.length
+    if (n == 0) {
+      return ary
+    }
+    if (!n) {
+      n = 1
+    }
+    if (n >= len) {
+      return res
+    }
+    for (var i = 0; i < len - n; i++) {
+      res.push(ary[i])
+    }
+    return res
+  },
+
+
+  dropRightWhile: function () {
+
+  },
+
+
+  dropWhile: function () {
+
+  },
+
+
+  fill: function (array, value, start = 0, end = array.length) {
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  },
+
+
+  findlndex: function (array, predicate = _.identity, fromIndex = 0) {
+
+  },
+
+
+  findLastlndex: function (array, predicate = _.identity, fromIndex = 0) {
+
+  },
+
+
+  flatten: function () {
+
+  },
+
+
+  flattenDeep: function () {
+
+  },
+
+
+  flattenDepth: function () {
+
+  },
+
+
+  fromPairs: function () {
+
+  },
+
+
+  head: function (array) {
+    var ary = array
+    if (!ary) {
+      return undefined
+    } else {
+      return ary[0]
+    }
+  },
+
+
+  indexOf: function (array, value, fromIndex = 0) {
+    for (var i = fromIndex; i < array.length; i++) {
+      if (array[i] == value) {
+        return i
+      }
+    }
+    return -1
+  },
+
+
+  initial: function (array) {
+    var res = []
+    for (var i = 0; i < array.length - 1; i++) {
+      res.push(array[i])
+    }
+    return res
+  },
+
+
+  join: function (array, separator = ',') {
+    var res = '' + array[i]
+    for (var i = 1; i < array.length; i++) {
+      res = res + separator + array[i]
+    }
+    return res
+  },
 }
