1b2f836f2edbd0de70361de0bc35ff7e4619aea2
jcy <1294164535@qq.com>
2022-01-12 22:46:12 +0800

    0112

--- lodash/zn_dyi-lodash.js
+++ lodash/zn_dyi-lodash.js
@@ -101,18 +101,75 @@ var zn_dyi = {
   },
 
 
-  flatten: function () {
+  flatten: function (array) {
+    return array.reduc((res, item) => {
+      return res.concat(item)
+    }, [])
 
+    // reduce 版
+    // return array.reduce((res, item) => {
+    //   if (Array.isArray(item)) {
+    //     res.push(...item)
+    //   } else {
+    //     res.push(item)
+    //   }
+    //   return res
+    // }, [])
+
+    // 常规版
+    // var res = []
+    // for (var i = 0; i < array.length; i++) {
+    //   var item = array[i]
+    //   if (Array.isArray(item)) {
+    //     res.push(...item)
+    //     // for (var j = 0; j < item.length; j++) {
+    //     //   res.push(item[j])
+    //     // }
+    //   } else {
+    //     res.push(item)
+    //   }
+    // }
+    // return res
   },
 
 
-  flattenDeep: function () {
+  flattenDeep: function (array) {
+    return array.reduce((res, item) => {
+      if (Array.isArray(item)) {
+        return res.concat(flattenDeep(item))
+      }
+      return res.concat(item)
+    }, [])
 
+    // 常规版
+    // var res = []
+    // for (var i = 0; i < array.length; i++) {
+    //   var item = array[i]
+    //   if (Array.isArray(item)) {
+    //     item = flattenDeep(item )
+    //     res.push(...item)
+    //     // for (var j = 0; j < item.length; j++) {
+    //     //   res.push(item[j])
+    //     // }
+    //   } else {
+    //     res.push(item)
+    //   }
+    // }
+    // return res
   },
 
 
-  flattenDepth: function () {
+  flattenDepth: function (array, depth = 1) {
+    if (depth == 0) {
+      return array.slice()
+    }
 
+    return array.reduce((res, item) => {
+      if (Array.isArray(item)) {
+        return res.concat(flattenDepth(item, depth - 1))
+      }
+      return res.concat(item)
+    }, [])
   },
 
 
@@ -156,5 +213,108 @@ var zn_dyi = {
       res = res + separator + array[i]
     }
     return res
+  },
+
+
+  last: function (array) {
+    return array[array.length - 1]
+  },
+
+
+  lastlndexOf: function (array, value, fromIndex = array.length) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      if (array[i] == value) {
+        return i
+      }
+    }
+    return -1
+  },
+
+
+  reverse: function (array) {
+    var i = 0
+    var j = array.length
+    if (j > i) {
+      var temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+      i++
+      j--
+    }
+    return array
+  },
+
+
+  uniq: function (array) {
+    var res = []
+    var map = {}
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] in map) {
+        map[array[i]]++
+      } else {
+        map[array[i]] = 1
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+
+  uniqBy: function (array, iteratee = _.identity) {
+    var res = []
+    var map = {}
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] in map) {
+        map[array[i]]++
+      } else {
+        map[array[i]] = 1
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+
+  // without: function (array, ...values) {
+  //   var res = []
+  //   for (var i = 0; i < array.length; i++) {
+  //     if (values.indexOf((array[i])) == -1) {
+  //       res.push(array[i])
+  //     }
+  //   }
+  //   return res
+  // },
+  // without: function (array, values) {
+  //   var res = []
+  //   var temp = []
+  //   temp.push(...arguments)
+  //   for (var i = 0; i < array.length; i++) {
+  //     if (array[i] in temp) {
+  //       continue
+  //     } else {
+  //       res.push(array[i])
+  //     }
+  //   }
+  //   return res
+  // },
+  without: function (array, values) {
+    var obj = {}
+    var res = []
+    for (var j = 1; j < arguments.length; j++) {
+      obj[arguments[i]] = 0
+    }
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] in obj) { // 数组的值在对象中没找到对应的下标位置，就push到res数组中。
+        continue
+      } else {
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+
+  zip: function (arrays) {
+
   },
 }
